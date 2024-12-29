a94bc6f18984f63e364f0c9047d24d121c509687
messagetomanju <messagetomanju@gmail.com>
2024-12-29 13:39:43 +0530

    bad robot moved as new module

--- main.py
+++ main.py
@@ -1,45 +1,67 @@
 import pygame
+from robot import Robot
+from bad_robot import BadRobot
 
 pygame.init()
 
-screen_width = 600
-screen_height = 600
+SCREEN_WIDTH = 600
+SCREEN_HEIGHT = 600
 
-#create screen
-# size argument in set_mode requires tuple - it is immutable - changed, modified
-#clock = pygame.time.Clock()
+# Create screen
+# size argument in set_mode requires tuple - it is immutable - changed ,modified
+screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+pygame.display.set_caption("RobotEscape")
 
-#create screen
-# size argument in set_mode requires tuple - it is immutable - changed, modified
-screen = pygame.display.set_mode((screen_width, screen_height))
+# Initialize robot object
+robot = Robot(x=100, y=100)
 
-robot_image = pygame.image.load("robo_image.png")
-IMAGE_SIZE = (30, 30)
-robot_image = pygame.transform.scale(robot_image,IMAGE_SIZE)
-robot_x = 5
-robot_y = 5
+# Initialize bad robot
+bad_robot = BadRobot()
+# bad_robot2 = BadRobot()
+# bad_robot3 = BadRobot()
 
+# Game scores and levels
+score = 0
+level = 0
 
 # infinite loop
 run = True
-while run:
-    #event handler
+while run: #GAME LOOP
+    #Event handler
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             run = False
-    keys = pygame.key.get_pressed()
-    if keys[pygame.K_LEFT]:
-        robot_x -= 1
-    if keys[pygame.K_RIGHT]:
-        robot_x += 1
-    if keys[pygame.K_UP]:
-        robot_y -= 1
-    if keys[pygame.K_DOWN]:
-        robot_y += 1
 
-    screen.fill((0,0,0))
-    screen.blit(robot_image, (robot_x,robot_y))
-    #screen.blit(robot_image, (40, 50))
+    # game logic
+    keys = pygame.key.get_pressed()
+
+    # Call the robot move method here
+    robot.move(keys)
+    robot.collides_with_sides(SCREEN_WIDTH, SCREEN_HEIGHT)
+
+    # Bad robot
+    bad_robot.appear_disappear()
+    bad_robot.collides_with_robot(robot)
+
+    # # Bad robot 2
+    # bad_robot2.appear_disappear()
+    # bad_robot2.collides_with_robot(robot)
+    #
+    # # Bad robot 3
+    # bad_robot3.appear_disappear()
+    # bad_robot3.collides_with_robot(robot)
+
+    screen.fill((255, 255, 255))
+    screen.blit(robot.image, (robot.x, robot.y))
+    if bad_robot.visible:
+        pygame.draw.rect(screen, bad_robot.color, bad_robot.rect)
+
+    # if bad_robot2.visible:
+    #     pygame.draw.rect(screen, bad_robot2.color, bad_robot2.rect)
+    #
+    # if bad_robot3.visible:
+    #     pygame.draw.rect(screen, bad_robot3.color, bad_robot3.rect)
+
     pygame.display.update()
 
 pygame.quit()
